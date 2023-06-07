b0ff64289ad55113f2851f198703c9c3cc18aa60
B198096-2022 <114732360+B198096-2022@users.noreply.github.com>
2023-06-07 14:17:42 +0100

    Update DF_scater_session_info.R

--- DF_scater_redo/DF_scater_session_info.R
+++ DF_scater_redo/DF_scater_session_info.R
@@ -1,1 +1,54 @@
-#Add session info
+
+R version 4.2.3 (2023-03-15)
+Platform: x86_64-apple-darwin17.0 (64-bit)
+Running under: macOS Monterey 12.6
+
+Matrix products: default
+LAPACK: /Library/Frameworks/R.framework/Versions/4.2/Resources/lib/libRlapack.dylib
+
+locale:
+[1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] ggplot2_3.4.2       DoubletFinder_2.0.3 patchwork_1.1.2     dplyr_1.1.1        
+[5] Seurat_4.3.0        SeuratObject_4.1.3  sp_1.6-0            fs_1.6.1           
+[9] remotes_2.4.2      
+
+loaded via a namespace (and not attached):
+  [1] nlme_3.1-162           matrixStats_0.63.0     spatstat.sparse_3.0-1 
+  [4] RcppAnnoy_0.0.20       RColorBrewer_1.1-3     httr_1.4.5            
+  [7] sctransform_0.3.5      tools_4.2.3            utf8_1.2.3            
+ [10] R6_2.5.1               irlba_2.3.5.1          KernSmooth_2.23-20    
+ [13] uwot_0.1.14            lazyeval_0.2.2         colorspace_2.1-0      
+ [16] withr_2.5.0            tidyselect_1.2.0       gridExtra_2.3         
+ [19] compiler_4.2.3         progressr_0.13.0       cli_3.6.1             
+ [22] spatstat.explore_3.1-0 plotly_4.10.1          scales_1.2.1          
+ [25] lmtest_0.9-40          spatstat.data_3.0-1    ggridges_0.5.4        
+ [28] pbapply_1.7-0          goftest_1.2-3          stringr_1.5.0         
+ [31] digest_0.6.31          spatstat.utils_3.0-2   pkgconfig_2.0.3       
+ [34] htmltools_0.5.5        parallelly_1.35.0      fastmap_1.1.1         
+ [37] htmlwidgets_1.6.2      rlang_1.1.1            rstudioapi_0.14       
+ [40] shiny_1.7.4            generics_0.1.3         zoo_1.8-12            
+ [43] jsonlite_1.8.4         spatstat.random_3.1-4  ica_1.0-3             
+ [46] magrittr_2.0.3         dotCall64_1.0-2        Matrix_1.5-4          
+ [49] Rcpp_1.0.10            munsell_0.5.0          fansi_1.0.4           
+ [52] abind_1.4-5            reticulate_1.28        lifecycle_1.0.3       
+ [55] stringi_1.7.12         MASS_7.3-58.3          Rtsne_0.16            
+ [58] plyr_1.8.8             grid_4.2.3             parallel_4.2.3        
+ [61] listenv_0.9.0          promises_1.2.0.1       ggrepel_0.9.3         
+ [64] deldir_1.0-6           miniUI_0.1.1.1         lattice_0.21-8        
+ [67] cowplot_1.1.1          splines_4.2.3          tensor_1.5            
+ [70] pillar_1.9.0           igraph_1.4.2           spatstat.geom_3.1-0   
+ [73] future.apply_1.10.0    reshape2_1.4.4         codetools_0.2-19      
+ [76] leiden_0.4.3           glue_1.6.2             data.table_1.14.8     
+ [79] spam_2.9-1             png_0.1-8              vctrs_0.6.2           
+ [82] httpuv_1.6.9           polyclip_1.10-4        gtable_0.3.3          
+ [85] RANN_2.6.1             purrr_1.0.1            tidyr_1.3.0           
+ [88] scattermore_0.8        future_1.32.0          mime_0.12             
+ [91] xtable_1.8-4           later_1.3.1            survival_3.5-5        
+ [94] viridisLite_0.4.2      tibble_3.2.1           cluster_2.1.4         
+ [97] globals_0.16.2         fitdistrplus_1.1-11    ellipsis_0.3.2        
+[100] ROCR_1.0-11   
