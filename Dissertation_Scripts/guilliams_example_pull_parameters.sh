8eeeb5b0a6c8ec2082329674a8b9261b110f789c
B198096-2022 <114732360+B198096-2022@users.noreply.github.com>
2023-06-15 16:33:53 +0200

    Create guilliams_example_pull_parameters.sh

---
+++ Dissertation_Scripts/guilliams_example_pull_parameters.sh
@@ -0,0 +1,5 @@
+big_dir=guilliams > pull_parameters.guilliams_healthy_cell
+sub_dir=guilliams_healthy_cell >> pull_parameters.guilliams_healthy_cell
+SRR_short=SRR173 >> pull_parameters.guilliams_healthy_cell
+SRR_long=SRR173750 >> pull_parameters.guilliams_healthy_cell
+logdir=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/max/cellranger/logs >> pull_parameters.guilliams_healthy_cell
