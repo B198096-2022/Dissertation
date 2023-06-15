1dca6a566707c0a31300d4c374ee14a0a4f437ef
B198096-2022 <114732360+B198096-2022@users.noreply.github.com>
2023-06-15 16:41:52 +0200

    Update guillimas_pull_cellranger.sh

--- Dissertation_Scripts/guillimas_pull_cellranger.sh
+++ Dissertation_Scripts/guillimas_pull_cellranger.sh
@@ -1,14 +1,19 @@
 #Guilliams
 
-ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR173/049/SRR17375049/SRR17375049_1.fastq.gz
 
+############################################################
+#                                                          #
+#        Now run the submit.jobs.sh script                 #
+#                                                          #
+############################################################
 
 #This is the final command
 #Run it in the scratch home space
 #Mkdir the big_dir for all of the data first
 #Both scripts used are in the data dir, but it is specified here
 #The script, the project name, and the pull parameters file
-./data/submit.pull.sh guilliams_healthy_cell pull_parameters.guilliams_healthy_cell
+
+#./data/submit.pull.sh guilliams_healthy_cell pull_parameters.guilliams_healthy_cell
 
 #Making the pull parameters file in home scratch dir
 #big_dir is the dir for the full dataset
@@ -16,22 +21,33 @@ ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR173/049/SRR17375049/SRR17375049_1.fastq.gz
 #SRR_short is the beginning of the SRR id that appears in the url
 #SRR_long is the repeating portion of the full SRR id in the  url,
 #       whatever is not specified by the project_numbers.txt
-pull_parameters.guilliams_healthy_cell
+
+##########################
+#  Healthy Cell samples  #
+##########################
+#pull_parameters.guilliams_healthy_cell
+
 echo big_dir=guilliams > pull_parameters.guilliams_healthy_cell
 echo sub_dir=guilliams_healthy_cell >> pull_parameters.guilliams_healthy_cell
 echo SRR_short=SRR173 >> pull_parameters.guilliams_healthy_cell
 echo SRR_long=SRR173750 >> pull_parameters.guilliams_healthy_cell
 echo logdir=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/max/cellranger/logs >> pull_parameters.guilliams_healthy_cell
 
+#Specify the ends of the SRR accessions on ENA 
+
 seq 32 33 >> guilliams_healthy_cell_numbers.txt
 seq 46 47 >> guilliams_healthy_cell_numbers.txt
 
+./data/submit.pull.sh guilliams_healthy_cell pull_parameters.guilliams_healthy_cell
 
 ######################
 
-./data/submit.pull.sh guilliams_fibrosis_cell pull_parameters.guilliams_fibrosis_cell
+###########################
+#  Fibrosis cell samples  #
+###########################
 
-pull_parameters.guilliams_fibrosis_cell
+
+#pull_parameters.guilliams_fibrosis_cell
 echo big_dir=guilliams > pull_parameters.guilliams_fibrosis_cell
 echo sub_dir=guilliams_fibrosis_cell >> pull_parameters.guilliams_fibrosis_cell
 echo SRR_short=SRR173 >> pull_parameters.guilliams_fibrosis_cell
@@ -42,11 +58,16 @@ echo 38 > guilliams_fibrosis_cell_numbers.txt
 seq 44 45 >> guilliams_fibrosis_cell_numbers.txt
 seq 48 50 >> guilliams_fibrosis_cell_numbers.txt
 
+./data/submit.pull.sh guilliams_fibrosis_cell pull_parameters.guilliams_fibrosis_cell
+
 ######################
 
-./data/submit.pull.sh guilliams_fibrosis_nuc pull_parameters.guilliams_fibrosis_nuc
+###########################
+#  Healthy Nuc  samples   #
+###########################
 
-pull_parameters.guilliams_fibrosis_nuc
+
+#pull_parameters.guilliams_fibrosis_nuc
 echo big_dir=guilliams > pull_parameters.guilliams_fibrosis_nuc
 echo sub_dir=guilliams_fibrosis_nuc >> pull_parameters.guilliams_fibrosis_nuc
 echo SRR_short=SRR173 >> pull_parameters.guilliams_fibrosis_nuc
@@ -56,11 +77,16 @@ echo logdir=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/max/cellranger/logs >>
 echo 11 > guilliams_fibrosis_nuc_numbers.txt
 seq 54 58 >> guilliams_fibrosis_nuc_numbers.txt
 
+./data/submit.pull.sh guilliams_fibrosis_nuc pull_parameters.guilliams_fibrosis_nuc
+
 ######################
 
-./data/submit.pull.sh guilliams_healthy_nuc pull_parameters.guilliams_healthy_nuc
+###########################
+#  Fibrosis Nuc  samples  #
+###########################
 
-pull_parameters.guilliams_fibrosis_nuc
+
+#pull_parameters.guilliams_fibrosis_nuc
 echo big_dir=guilliams > pull_parameters.guilliams_healthy_nuc
 echo sub_dir=guilliams_healthy_nuc >> pull_parameters.guilliams_healthy_nuc
 echo SRR_short=SRR173 >> pull_parameters.guilliams_healthy_nuc
@@ -69,55 +95,15 @@ echo logdir=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/max/cellranger/logs >>
 
 seq 51 53 >> guilliams_healthy_nuc_numbers.txt
 
+./data/submit.pull.sh guilliams_healthy_nuc pull_parameters.guilliams_healthy_nuc
 
-
-
-######################
-
-submit.pull.sh
-#!/bin/bash
-jid=$1
-parameters=$2
-
-while IFS='=' read -a pArray; do
-  p=${pArray[0]}
-  declare ${p}="${pArray[1]}"
-done < ${parameters}
-
-qsub -N pull.${jid} -cwd -l h_vmem=32g -V -v parameters=${parameters} -j y -o ${logdir}/ /exports/eddie/scratch/s2249132/data/data_pull.sh
-
-######################
-data_pull.sh
-#!/bin/bash
-
-while IFS='=' read -a pArray; do
-  p=${pArray[0]}
-  declare ${p}="${pArray[1]}"
-done < ${parameters}
-
-count=$((1))
-
-while read id; do
-  count2=${id: -2}
-  mkdir /exports/eddie/scratch/s2249132/data/${big_dir}/${sub_dir}_${count}
-  cd /exports/eddie/scratch/s2249132/data/${big_dir}/${sub_dir}_${count}
-  wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR_short}/0${count2}/${SRR_long}${id}/${SRR_long}${id}_1.fastq.gz
-  mv ${SRR_long}${id}_1.fastq.gz ${SRR_long}${id}_S1_L001_R1_001.fastq.gz
-  wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR_short}/0${count2}/${SRR_long}${id}/${SRR_long}${id}_2.fastq.gz
-  mv ${SRR_long}${id}_2.fastq.gz ${SRR_long}${id}_S1_L001_R2_001.fastq.gz
-  cd /exports/eddie/scratch/s2249132/data/${big_dir}
-  echo stage=0 > params.${sub_dir}_${count}
-  echo tidy=0 >> params.${sub_dir}_${count}
-  echo dir=/exports/eddie/scratch/s2249132/data/${big_dir} >> params.${sub_dir}_${count}
-  echo project=${sub_dir}_${count} >> params.${sub_dir}_${count}
-  echo n=0 >> params.${sub_dir}_${count}
-  echo ref=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/references/refdata-gex-GRCh38-2020-A/ >> params.${sub_dir}_${count}
-  echo logdir=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/max/cellranger/logs >> params.${sub_dir}_${count}
-  echo destination=/exports/cmvm/datastore/scs/groups/pramacha-GROUP/Max_Hammer/cellranger/${big_dir} >> params.${sub_dir}_${count}
-  echo srcdir=/exports/cmvm/eddie/scs/groups/pramacha-GROUP/max/cellranger/src >> params.${sub_dir}_${count}
-  count=$((count+1))
-done < ${sub_dir}_numbers.txt
-
+######################################################################
+#                                                                    #
+#              Now run the submit.jobs.sh script                     #
+#                                                                    #
+# Requires specifying the .src/ directory where scripts are located  #
+#                                                                    #
+######################################################################
 
 #To run cell cellranger
 #Move to the main scratch home
